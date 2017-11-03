5f9279556ed5b53112a4f51e7059427e329bd99d
Chris Kellendonk <chriskellendonk@gmail.com>
2017-11-03 00:23:17 -0400

    Setup simple libuv server

--- src/server.c
+++ src/server.c
@@ -30,6 +30,12 @@ do { \
   } \
 } while(0)
 
+#define HTTP_HEADER "HTTP/1.1 200 OK\r\n"           \
+                    "Content-Type: text/html\r\n"   \
+                    "Content-Length: 20\r\n"        \
+                    "\r\n"                          \
+                    "Hello from Brightray"
+
 #define MAX_WRITE_HANDLES 1000
 #define MAX_HTTP_HEADERS 20
 
@@ -229,18 +235,151 @@ void on_connect(uv_stream_t * server_handle, int status) {
   }
 }
 
+void on_get_write(uv_write_t *req, int status){
+  http_request_t * http_request = req->data;
+  
+  UV_CHECK(status, "on_get_write");
+
+  if (http_request->url != NULL) {
+      free(http_request->url);
+      http_request->url = NULL;
+  }
+
+  if (http_request->method != NULL) {
+      free(http_request->method);
+      http_request->method = NULL;
+  }
+
+  for (int i = 0; i < http_request->header_lines; ++i) {
+      http_header_t * header = &http_request->headers[i];
+
+      if (header->field != NULL) {
+          free(header->field);
+          header->field = NULL;
+      }
+      if (header->value != NULL) {
+          free(header->value);
+          header->value = NULL;
+      }
+  }
+
+  if (!uv_is_closing((uv_handle_t *)req->handle)) {
+      uv_close((uv_handle_t *) req->handle, on_close);
+  }
+}
+
+int parser_on_message_begin(http_parser * parser) {
+  DEBUG_PRINT("Message Begin");
+
+  http_request_t * http_request = parser->data;
+  http_request->header_lines = 0;
+
+  return 0;
+}
+
+int parser_on_url(http_parser * parser, const char * at, size_t length) {
+  DEBUG_PRINT("Url: %.*s", (int) length, at);
+
+  http_request_t * http_request = parser->data;
+
+  http_request->url = malloc(length + 1);
+
+  strncpy(http_request->url, at, length);
+  http_request->url[length] = '\0';
+
+  return 0;
+}
+
+int parser_on_header_field(http_parser * parser, const char * at, size_t length) {
+  DEBUG_PRINT("Header field: %.*s", (int)length, at);
+
+  http_request_t * http_request = parser->data;
+  http_header_t * header = &http_request->headers[http_request->header_lines];
+
+  header->field = malloc(length + 1);
+  header->field_length = length;
+
+  strncpy(header->field, at, length);
+  header->field[length] = '\0';
+
+  return 0;
+}
+
+int parser_on_header_value(http_parser * parser, const char * at, size_t length) {
+  DEBUG_PRINT("Header value: %.*s", (int)length, at);
+
+  http_request_t * http_request = parser->data;
+  http_header_t * header = &http_request->headers[http_request->header_lines];
+
+  header->value = malloc(length + 1);
+  header->value_length = length;
+
+  strncpy(header->value, at, length);
+  header->value[length] = '\0';
+
+  http_request->header_lines += 1;
+
+  return 0;
+}
+
+int parser_on_headers_complete(http_parser * parser) {
+  DEBUG_PRINT("Headers Complete");
+
+  http_request_t * http_request = parser->data;
+
+  const char * method = http_method_str((enum http_method) parser->method);
+  
+  http_request->method = malloc(sizeof(method));
+  strncpy(http_request->method, method, strlen(method));
+  
+  return 0;
+}
+
+int parser_on_body(http_parser * parser, const char * at, size_t length) {
+  DEBUG_PRINT("Body: %.*s", (int) length, at);
+
+  http_request_t * http_request = parser->data;
+
+  http_request->body = malloc(length + 1);
+  http_request->body_length = length;
+
+  strncpy(http_request->body, at, length);
+  http_request->body[length] = '\0';
+
+  return 0;
+}
+
+int parser_on_message_complete(http_parser * parser) {
+  DEBUG_PRINT("Message Complete");
+
+  http_request_t *http_request = parser->data;
+
+  // Set Header
+  http_request->resp_buf[0].base = HTTP_HEADER;
+  http_request->resp_buf[0].len = sizeof(HTTP_HEADER) - 1;
+
+  uv_write(
+    &http_request->req, 
+    &http_request->stream, 
+    http_request->resp_buf, 
+    1, 
+    on_get_write);
+
+  return 0;
+}
+
 int br_server_run(br_server * br) {
   struct sockaddr_in address;
 
   signal(SIGPIPE, SIG_IGN);
 
-  /*parser_settings.on_message_begin = on_message_begin;
-  parser_settings.on_url = on_url;
-  parser_settings.on_header_field = on_header_field;
-  parser_settings.on_header_value = on_header_value;
-  parser_settings.on_headers_complete = on_headers_complete;
-  parser_settings.on_body = on_body;
-  parser_settings.on_message_complete = on_message_complete;*/
+  parser_settings.on_message_begin = parser_on_message_begin;
+  parser_settings.on_url = parser_on_url;
+  parser_settings.on_header_field = parser_on_header_field;
+  parser_settings.on_header_value = parser_on_header_value;
+  parser_settings.on_headers_complete = parser_on_headers_complete;
+  parser_settings.on_body = parser_on_body;
+  parser_settings.on_message_complete = parser_on_message_complete;
 
   uv_loop = uv_default_loop();
 
